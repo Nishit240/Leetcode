c18088b9602c7f1d911f0850b9b1bf5b7ef42d03
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-02 12:06:35 +0530

    Time: 1 ms (72.59%) | Memory: 13.3 MB (89.36%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -22,7 +22,7 @@ class Solution(object):
         while j < len(nums):
             if nums[j] != nums[i]:
                 i += 1
-                nums[i], nums[j] = nums[j], nums[i]
+                nums[i] = nums[j]
             j +=1
         return i+1
 
