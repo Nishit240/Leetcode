9169af4bee4a41310da6034493e8c92659ca8cb3
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-03 12:20:39 +0530

    Time: 7 ms (98.17%) | Memory: 13.3 MB (38.97%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -10,29 +10,16 @@ class Solution(object):
         # s = s.replace(" ", "")
         # s = s.replace(",", "")
         # s = s.replace(":", "")
-        # s = s.lower()
-        # p = s[::-1]
 
-        # if s == p:
+        # if s.lower() == s[::-1].lower():
         #     return True
         # else:
         #     return False
 
-        i, j = 0, len(s) - 1
-
-        while i < j:
-            while i < j and not s[i].isalnum():
-                i += 1
-            while i < j and not s[j].isalnum():
-                j -= 1
-
-            if s[i].lower() != s[j].lower():
-                return False
-
-            i += 1
-            j -= 1
-
-        return True
+        s= ''.join([char for char in s if char.isalnum()])
+        if s[::-1].lower() == s.lower():
+            return True 
+        return False 
 
 
         
