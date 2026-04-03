85ebccc91d85649ef1db6fa6e9f4fdd330491da3
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-03 11:57:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (66.70%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,30 +1,39 @@
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
-        nums3 = []
-        i, j = 0, 0
-        n, m = len(nums1), len(nums2)
+        # nums3 = []
+        # i, j = 0, 0
+        # n, m = len(nums1), len(nums2)
 
-        # Merge step
-        while i < n and j < m:
-            if nums1[i] <= nums2[j]:
-                nums3.append(nums1[i])
-                i += 1
-            else:
-                nums3.append(nums2[j])
-                j += 1
+        # # Merge step
+        # while i < n and j < m:
+        #     if nums1[i] <= nums2[j]:
+        #         nums3.append(nums1[i])
+        #         i += 1
+        #     else:
+        #         nums3.append(nums2[j])
+        #         j += 1
 
-        # Remaining elements
-        while i < n:
-            nums3.append(nums1[i])
-            i += 1
+        # # Remaining elements
+        # while i < n:
+        #     nums3.append(nums1[i])
+        #     i += 1
 
-        while j < m:
-            nums3.append(nums2[j])
-            j += 1
+        # while j < m:
+        #     nums3.append(nums2[j])
+        #     j += 1
 
-        # Median calculation
-        k = len(nums3)
-        if k % 2 == 1:
-            return float(nums3[k // 2])
+        # # Median calculation
+        # k = len(nums3)
+        # if k % 2 == 1:
+        #     return float(nums3[k // 2])
+        # else:
+        #     return (nums3[k // 2 - 1] + nums3[k // 2]) / 2.0
+
+        mergedList = nums1 + nums2
+        sortedList = sorted(mergedList)
+        n = len(sortedList)
+
+        if n % 2 == 0:
+            return (sortedList[n//2 - 1] + sortedList[n//2]) / 2.0
         else:
-            return (nums3[k // 2 - 1] + nums3[k // 2]) / 2.0
+            return sortedList[n//2]
