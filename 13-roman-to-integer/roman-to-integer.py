da20721a7697fee4a3b76c4da4da8ee2504c0f62
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-03 12:36:26 +0530

    Time: 23 ms (5.96%) | Memory: 12.2 MB (95.28%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -5,10 +5,10 @@ class Solution(object):
         :rtype: int
         """
         letter = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50 , 'C' : 100, 'D' : 500, 'M' : 1000}
-        n = len(s)
+
         result = 0
-        for i in range (n):
-            if i < n-1 and letter[s[i]] < letter[s[i+1]]:
+        for i in range (len(s)):
+            if i < len(s)-1 and letter[s[i]] < letter[s[i+1]]:
                 result -= letter[s[i]]
             else: 
                 result += letter[s[i]]
