4b80bf149aa799128ba752a4bc5a3f76a92dbff9
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-04 13:30:25 +0530

    Time: 33 ms (89.36%) | Memory: 19 MB (60.96%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -12,12 +12,21 @@ class Solution(object):
         #             mp = max(mp,p)
         # return mp
 
-        mp = 0
-        minp = 10**5
-        for i in range(len(prices)):
-            minp = min(minp, prices[i])
-            mp = max(mp,prices[i] - minp)
-        return mp
+        # mp = 0
+        # minp = 10**5
+        # for i in range(len(prices)):
+        #     minp = min(minp, prices[i])
+        #     mp = max(mp,prices[i] - minp)
+        # return mp
+
+        s = float('inf')
+        p = 0
+        for i in prices:
+            if i < s:
+                s = i
+            elif i - s > p:
+                p = i - s
+        return p
 
 
 
