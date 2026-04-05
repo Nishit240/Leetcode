366c5e5eff06744a77f45d3de38086b0145d3257
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-05 14:56:43 +0530

    Time: 50 ms (31.65%) | Memory: 19 MB (99.46%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,11 +1,27 @@
+# class Solution:
+#     def reverse(self, x: int) -> int:
+#         # Step 1: Handle the sign and reverse the digits using string slicing
+#         sign = -1 if x < 0 else 1
+#         res = int(str(abs(x))[::-1]) * sign
+        
+#         # Step 2: Check for 32-bit integer overflow
+#         if res < -2**31 or res > 2**31 - 1:
+#             return 0
+            
+#         return res
+
 class Solution:
     def reverse(self, x: int) -> int:
-        # Step 1: Handle the sign and reverse the digits using string slicing
-        sign = -1 if x < 0 else 1
-        res = int(str(abs(x))[::-1]) * sign
-        
-        # Step 2: Check for 32-bit integer overflow
-        if res < -2**31 or res > 2**31 - 1:
+        if x<=-(1<<31) or x>=(1<<31)-1:
             return 0
             
-        return res
+        if x>0:
+            a=int(str(x)[::-1])
+            if  a>(1<<31)-1:
+                return 0
+            return int(str(x)[::-1])
+        else:
+            a=-int(str(-x)[::-1])
+            if a<-(1<<31):
+                return 0
+            return -int(str(-x)[::-1])
