9701745fa9b86ba3eb2a1997ad88aa697a6195bc
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-06 13:17:34 +0530

    Time: 5 ms (83.89%) | Memory: 12.4 MB (68.62%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,12 +4,28 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
+        # if x < 0:
+        #     return False
+        
+        # rev = int(str(x)[::-1])
+        
+        # if x == rev:
+        #     return True
+        # else:
+        #     return False   
+
         if x < 0:
             return False
         
-        rev = int(str(x)[::-1])
-        
-        if x == rev:
+        s = str(x)
+        return self.helper(s, 0, len(s) - 1)
+
+    def helper(self, s, l, r):
+        if l >= r:
             return True
-        else:
-            return False        
+        
+        if s[l] != s[r]:
+            return False
+        
+        return self.helper(s, l + 1, r - 1)
+        
