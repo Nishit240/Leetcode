062899a07c205c8616167d49fd09d1cca34f1191
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-07 16:49:37 +0530

    Time: 771 ms (40.80%) | Memory: 12.8 MB (96.61%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,24 +1,32 @@
 class Solution(object):
     def twoSum(self, nums, target):
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        # n = len(nums)
-        # for i in range(0 , n-1):
-        #     for j in range (i+1, n):
-        #         if nums[i] + nums[j] == target:
-        #             return[i,j]
-
         n = len(nums)
-        hash_map = {}
+        for i in range(0 , n-1):
+            for j in range (i+1, n):
+                if nums[i] + nums[j] == target:
+                    return[i,j]
+        """
 
-        for i in range(n):
-            remaning = target - nums[i]
-            if remaning in hash_map:
-                return [hash_map[remaning], i]
-            hash_map[nums[i]] = i 
+        # n = len(nums)
+        # hash_map = {}
+
+        # for i in range(n):
+        #     remaning = target - nums[i]
+        #     if remaning in hash_map:
+        #         return [hash_map[remaning], i]
+        #     hash_map[nums[i]] = i 
+
+        i = 0
+        while i < len(nums):
+            two = target - nums[i]
+            if two in nums:
+                if nums.index(two) < i:
+                    return [nums.index(two), i]
+                elif nums.index(two) > i:
+                    return [i, nums.index(two)]
+            i += 1
+        return []
 
 
 
