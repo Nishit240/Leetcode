4ed61899c7aab5e34876244ff0d2834e3ba82386
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-08 17:02:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (34.99%) - LeetSync

---
+++ 48-rotate-image/rotate-image.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        """
+        n = len(matrix)
+        result = [[0 for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                result[j][(n-1)-i] = matrix[i][j]
+        for i in range(n):
+            for j in range(n):
+                matrix[i][j] = result[i][j]
+        
+
+            
+
+
+
+        
