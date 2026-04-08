e0688c6d23d4b9e64e40131d40404f9b77d8678a
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-08 16:07:22 +0530

    Time: 4 ms (17.06%) | Memory: 13 MB (28.43%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.py
+++ 898-transpose-matrix/transpose-matrix.py
@@ -1,16 +1,14 @@
 class Solution(object):
     def transpose(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        r = len(matrix)
-        c = len(matrix[0])
-        result = [[0]*r for _ in range(c)]
-
-        for i in range(r):
-            for j in range(c):
-                result[j][i] = matrix[i][j]
+        rows = len(matrix)
+        cols = len(matrix[0])
+        
+        result = []
+        
+        for j in range(cols):
+            new_row = []
+            for i in range(rows):
+                new_row.append(matrix[i][j])
+            result.append(new_row)
         return result
-                
         
