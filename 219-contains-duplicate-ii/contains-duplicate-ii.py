933f069ca45acdc4a309bdf921f04e4c8fa8592f
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-10 18:49:37 +0530

    Time: 42 ms (45.46%) | Memory: 25 MB (38.96%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -4,17 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :type k: int
         :rtype: bool
+
         """
-        my_set = set()
-        l = r = 0
-        for num in nums:
-            if num in my_set:
-                return True
-            my_set.add(num)
-            if len(my_set) > k:
-                my_set.remove(nums[l])
-                l += 1
-        return False
+        d = {}
+        for i in range(len(nums)):
+            if nums[i] in d:
+                if i - d[nums[i]] <= k:
+                    return True
+            d[nums[i]] = i
+        
+        return False 
+        # my_set = set()
+        # l = r = 0
+        # for num in nums:
+        #     if num in my_set:
+        #         return True
+        #     my_set.add(num)
+        #     if len(my_set) > k:
+        #         my_set.remove(nums[l])
+        #         l += 1
+        # return False
             
 
 
