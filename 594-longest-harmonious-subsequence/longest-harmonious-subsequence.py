cf940497ceb7f0cdd0e5d073a3fd4778ef48353e
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-11 11:53:53 +0530

    Time: 31 ms (85.20%) | Memory: 14.4 MB (58.11%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
@@ -1,11 +1,29 @@
 class Solution(object):
     def findLHS(self, nums):
+        '''
         nums = Counter(nums)
         res = 0
         for i in nums:
             if i+1 in nums:
                 res = max(res, (nums[i] + nums[i+1]))
         return res
+        '''
+        my_dict = {}
+        for element in nums:
+            if element not in my_dict:
+                my_dict[element] = 1
+            else:
+                my_dict[element] += 1
+        max = 0
+        for key in my_dict:
+            if (key + 1 in my_dict) and (my_dict[key] + my_dict[key + 1] > max):
+                max = (my_dict[key] + my_dict[key + 1])
+        return max
+
+
+
+
+
             
 
              
