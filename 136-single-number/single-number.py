611dfc03bee65172045641e5017d23127e7c46fe
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-13 13:57:51 +0530

    Time: 4 ms (51.89%) | Memory: 13.9 MB (83.95%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        freq = {}
+
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
+        for num in freq:
+            if freq[num] == 1:
+                return num
+        """
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
