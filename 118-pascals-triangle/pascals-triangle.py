80f4bd1023b3106eea5525e684e24db907617319
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-14 22:02:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (36.26%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result = [[1]]
+        for i in range(numRows - 1):
+            temp = [0] + result[-1] + [0]
+            current = []
+            for j in range(len(result[-1]) + 1):
+                current.append(temp[j] + temp[j + 1])
+            result.append(current)
+        return result         
