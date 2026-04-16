1b91ee056a64dad8fbe368118a04d52271d6e484
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-16 21:53:37 +0530

    Time: 13 ms (34.66%) | Memory: 13.6 MB (78.70%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,22 +1,23 @@
 class Solution(object):
     def majorityElement(self, nums):
         """
-        :type nums: List[int]
-        :rtype: int
-        """
         candidate = 0
         count = 0
-
         for num in nums:
-
             if count == 0:
                 candidate = num
-
             if candidate == num:
                 count += 1
             else:
                 count -= 1
-
         return candidate
+        """
+        hashmap = {}
+        for i in nums:
+            hashmap[i] = hashmap.get(i,0) + 1 
+
+        for i in nums:
+            if hashmap[i] > len(nums) // 2:
+                return i
 
         
