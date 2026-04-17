070b66637c02de7e6f1fb88d902192cb0ecb3fa7
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-17 20:32:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (34.52%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,8 +1,6 @@
 class Solution:
     def generate(self, numRows):
         """
-        :type numRows: int
-        :rtype: List[List[int]]
         result = [[1]]
         for i in range(numRows - 1):
             temp = [0] + result[-1] + [0]
@@ -12,15 +10,10 @@ class Solution:
             result.append(current)
         return result         
         """
-
         res = []
-
         for i in range(numRows):
             row = [1] * (i + 1)
-
             for j in range(1, i):
                 row[j] = res[i-1][j-1] + res[i-1][j]
-
             res.append(row)
-
         return res
