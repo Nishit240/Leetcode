828e0dd8c26daf4a4a803f4bc4e938bf5ef55693
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-18 18:55:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (65.71%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,10 +1,8 @@
 class Solution(object):
     def addBinary(self, a, b):
         """
-        :type a: str
-        :type b: str
-        :rtype: str
-        """
         sum_int = int(a, 2) + int(b, 2)
-        
         return bin(sum_int)[2:]
+        """
+        result = bin(int(a, 2) + int(b, 2))[2:]
+        return result
