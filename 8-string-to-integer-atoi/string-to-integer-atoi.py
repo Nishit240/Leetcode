25cfbaecbe8369a59a4d706242615c4c0f3f9792
Nishit Jain <115970679+Nishit240@users.noreply.github.com>
2026-04-19 19:44:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (30.72%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def myAtoi(self, s):
         """
-        :type s: str
-        :rtype: int
-        """
         s = s.lstrip()
 
         if not s:
@@ -34,6 +31,36 @@ class Solution(object):
             return -2**31
         else:
             return p
+        """
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+        
+        i, n = 0, len(s)
+        
+        # 1. Skip leading spaces
+        while i < n and s[i] == ' ':
+            i += 1
+        
+        # 2. Handle sign
+        sign = 1
+        if i < n and (s[i] == '+' or s[i] == '-'):
+            if s[i] == '-':
+                sign = -1
+            i += 1
+        
+        # 3. Convert digits
+        num = 0
+        while i < n and s[i].isdigit():
+            digit = ord(s[i]) - ord('0')
+            
+            # 4. Overflow check BEFORE multiplying
+            if num > INT_MAX // 10 or (num == INT_MAX // 10 and digit > 7):
+                return INT_MAX if sign == 1 else INT_MIN
+            
+            num = num * 10 + digit
+            i += 1
+        
+        return sign * num
 
 
         
